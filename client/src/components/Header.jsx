b149428f198345f219276de83b6bf3b46853d0c3
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-08 16:55:44 +0530

    feat: Add Slider component and update Header
    This commit adds the Slider component to the client application and updates the Header component to include the Slider and a secondary call-to-action button. The Slider component will be responsible for displaying a slideshow of images. This change enhances the user experience by providing more interactive content on the header section of the page.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,6 +3,9 @@ import Grid from '../assets/Grid.png'
 import Girl_Image from '../assets/girl_image.png'
 import bag_logo from '../assets/bag-logo.png'
 import Rocket from '../assets/Rocket.png'
+import sparks from '../assets/sparks.png'
+import Slider from './Slider'
+import Secondary_CTA from './Secondary_CTA'
 
 const Header = () => {
     return (
@@ -23,22 +26,48 @@ const Header = () => {
                 <div className='flex gap-2 items-center justify-center'>
                     <div className='relative'>
                         <img className='rounded-3xl' src={Girl_Image} />
+
                         <div className='bg-white rounded-full w-20 h-20 flex items-center justify-center absolute top-16 right-6'>
                             <img src={bag_logo} />
                         </div>
-                        <div>
-                            <div className='flex gap-2 bg-white rounded-full'>
-                                <div className='h-12 w-12 bg-[#DDEFE0] rounded-full flex justify-center items-center'>
+
+                        <div className='absolute bottom-10 -left-14'>
+                            <div className='flex gap-2 bg-white rounded-full p-3 drop-shadow-xl'>
+                                <div className='h-14 w-14 bg-[#DDEFE0] rounded-full flex justify-center items-center'>
                                     <img src={Rocket} />
                                 </div>
-                                <div>
-                                    <p className='manrope font-[800]'>10 Days</p>
-                                    <p>Staff Deployment</p>
+                                <div className='px-3'>
+                                    <p className='font-bold text-2xl'>10 Days</p>
+                                    <p className='text-[#828282]'>Staff Deployment</p>
                                 </div>
                             </div>
                         </div>
+
+                        <div className='bg-[#002E18] absolute p-10 rounded-3xl flex flex-col gap-4 -bottom-12 -end-28 border-s border-t border-slate-700'>
+                            <div className='flex gap-2 items-end'>
+                                <p className='text-white font-medium text-5xl'>$0.5</p>
+                                <p className='text-[#A6A3A0] text-xl uppercase'>Million</p>
+                            </div>
+                            <div className='text-[#CCCCCC] font-medium max-w-52 text-lg'>
+                                Reduced client expenses by saving on hiring and employee costs.
+                            </div>
+                        </div>
+
+                        <div className='absolute bg-white rounded-3xl p-10 drop-shadow-lg top-24 -left-36'>
+                            <div className='relative'>
+                                <img className='absolute -top-16 -left-10' src={sparks} />
+                                <h1 className='font-semibold text-5xl'>40%</h1>
+                            </div>
+                            <div className='font-medium text-[#828282] max-w-52 text-xl'>
+                                Achieved reduction in project execution time by optimising team availability
+                            </div>
+                        </div>
                     </div>
-                    <div>second</div>
+
+                    <div>
+                        <Slider />
+                        <Secondary_CTA text={"Explore More"} />
+                    </div>
                 </div>
             </div>
         </div>
